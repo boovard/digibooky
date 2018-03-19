d2d9b774ea1dbf570dc8c3a486ef62bbc473dea0
reneald <delea.vanreeth@gmail.com>
2018-03-19 16:10:41 +0100

    completed 1st PersonRepo test

--- domain/src/test/java/be/thebest/domain/repositories/PersonRepositoryTest.java
+++ domain/src/test/java/be/thebest/domain/repositories/PersonRepositoryTest.java
@@ -1,14 +1,15 @@
 package be.thebest.domain.repositories;
 
-import be.thebest.domain.objects.Persons.Admin;
-import be.thebest.domain.objects.Persons.Person;
+import be.thebest.domain.objects.persons.Admin;
+import be.thebest.domain.objects.persons.Person;
 import org.assertj.core.api.Assertions;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.UUID;
 
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.entry;
 
 public class PersonRepositoryTest {
     PersonRepository testRepo;
@@ -20,10 +21,11 @@ public class PersonRepositoryTest {
 
     @Test
     public void addPerson_whenAnyPerson_shouldAddNewPersonToHashMap() {
-        Person testAdmin = new Admin(UUID.randomUUID(), "Code", "Mike", "mike.code@gmail.com");
+        UUID testId = UUID.randomUUID();
+        Person testAdmin = new Admin(testId, "Code", "Mike", "mike.code@gmail.com");
 
         testRepo.addPerson(testAdmin);
-        Assertions.assertThat(testRepo.getPersonRepository).contains(testAdmin);
+        assertThat(testRepo.getPersonRepository()).contains(entry(testId, testAdmin));
 
     }
 
