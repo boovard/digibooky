770c292450eec35d0390fb5e3e924468cfe83e92
MarieLynneBlock <marielynne.block@gmail.com>
2018-03-19 16:04:13 +0100

    Creating a book repo

--- domain/src/main/java/be/thebest/domain/repositories/BookRepository.java
+++ domain/src/main/java/be/thebest/domain/repositories/BookRepository.java
@@ -1,4 +1,31 @@
 package be.thebest.domain.repositories;
 
+import be.thebest.domain.objects.Book;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 public class BookRepository {
+    private Map<String, Book> bookRepository;
+
+    public BookRepository() {
+        this.bookRepository = new HashMap<>();
+    }
+
+    public Map<String, Book> getBookRepository() {
+        return Collections.unmodifiableMap(bookRepository);
+    }
+
+    public Map<String, Book> getAllBooks() {
+        return null;
+    }
+
+    public String getBookDetails() {
+        return null;
+    }
+
+    public Book searchOnIsbn() {
+        return null;
+    }
 }
