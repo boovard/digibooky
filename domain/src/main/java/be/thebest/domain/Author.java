3a3b7610b19d511e17d22c7a4d1ff63a577f9d4a
Block Marie-Lynne (100) <Marie-Lynne.Block@cmc.be>
2018-03-19 14:38:46 +0100

    Create base structure classes

---
+++ domain/src/main/java/be/thebest/domain/Author.java
@@ -0,0 +1,20 @@
+package be.thebest.domain;
+
+public class Author {
+    private String lastName;
+    private String firstName;
+
+    public Author(String lastName, String firstName) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+}
