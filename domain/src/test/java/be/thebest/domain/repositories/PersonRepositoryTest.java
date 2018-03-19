defc96c984acf2d1cda634e5741913d85ad5d1c3
reneald <delea.vanreeth@gmail.com>
2018-03-19 15:58:17 +0100

    added 1st PersonRepo test

--- domain/src/test/java/be/thebest/domain/repositories/PersonRepositoryTest.java
+++ domain/src/test/java/be/thebest/domain/repositories/PersonRepositoryTest.java
@@ -1,7 +1,12 @@
 package be.thebest.domain.repositories;
 
+import be.thebest.domain.objects.Persons.Admin;
+import be.thebest.domain.objects.Persons.Person;
+import org.assertj.core.api.Assertions;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.UUID;
 
 import static org.junit.Assert.*;
 
@@ -14,7 +19,11 @@ public class PersonRepositoryTest {
     }
 
     @Test
-    public void addPerson_whenGivenAllNecessaryArguments_shouldAddNewPersonToHashMap() {
+    public void addPerson_whenAnyPerson_shouldAddNewPersonToHashMap() {
+        Person testAdmin = new Admin(UUID.randomUUID(), "Code", "Mike", "mike.code@gmail.com");
+
+        testRepo.addPerson(testAdmin);
+        Assertions.assertThat(testRepo.getPersonRepository).contains(testAdmin);
 
     }
 
