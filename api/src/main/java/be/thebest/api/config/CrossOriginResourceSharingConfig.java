06e79dc8a8f330dc4e405befe78878a31fd36196
nielsdelestinne <nielsdelestinne@gmail.com>
2018-03-20 16:19:50 +0100

    Allowed Cross Origin

---
+++ api/src/main/java/be/thebest/api/config/CrossOriginResourceSharingConfig.java
@@ -0,0 +1,36 @@
+package be.thebest.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Enables Cross Origin Resource Sharing.
+ * Meaning: an application that's running on a different domain (origin) will be now
+ * able to access the resources (REST endpoints) exposed by this back-end application.
+ * If we don't enable this, our GUI (running on a different port and thus origin) will not
+ * be able to access the REST endpoint.
+ *
+ * Will be component scanned!
+ * Press F4 on this annotation. You'll see it's annotated as @Component (@Named)
+ */
+@Configuration
+public class CrossOriginResourceSharingConfig {
+
+    private static final String ALLOWED_ORIGINS = "*";
+    private static final String ALLOWED_PATH_MAPPINGS = "/**";
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping(ALLOWED_PATH_MAPPINGS)
+                        .allowedOrigins(ALLOWED_ORIGINS);
+            }
+        };
+    }
+
+}
