5b73be45125ff02fb0986772a32192c8a649753b
reneald <delea.vanreeth@gmail.com>
2018-03-20 16:23:11 +0100

    implementation of PersonController (get Members) (without authorisation check)

--- api/src/main/java/be/thebest/api/MemberDto.java
+++ api/src/main/java/be/thebest/api/MemberDto.java
@@ -1,4 +1,67 @@
 package be.thebest.api;
 
+import be.thebest.domain.objects.persons.Address;
+
+import java.util.UUID;
+
 public class MemberDto {
+    private UUID uuid;
+    private String inss;
+    private String lastName;
+    private String firstName;
+    private String email;
+    private Address address;
+
+    public static MemberDto memberDto() {
+        return new MemberDto();
+    }
+
+    public MemberDto withUUID(UUID uuid) {
+        this.uuid = uuid;
+        return this;
+    }
+
+    public MemberDto withLastName(String lastName) {
+        this.lastName = lastName;
+        return this;
+    }
+
+    public MemberDto withFirstName(String firstName) {
+        this.firstName = firstName;
+        return this;
+    }
+
+    public MemberDto withEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public MemberDto withAddress(Address address) {
+        this.address = address;
+        return this;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public String getInss() {
+        return inss;
+    }
 }
--- service/src/main/java/be/thebest/service/PersonService.java
+++ service/src/main/java/be/thebest/service/PersonService.java
@@ -32,6 +32,6 @@ public class PersonService {
     }
 
     private void verifyPermission() {
-
+        //TODO to be implemented
     }
 }
