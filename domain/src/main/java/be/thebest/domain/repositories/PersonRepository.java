cf577f360ec7ffae63a0d054f6cbe3c86aa360da
reneald <delea.vanreeth@gmail.com>
2018-03-20 08:38:33 +0100

    added placeholder add methods for PersonRepository

--- domain/src/main/java/be/thebest/domain/repositories/PersonRepository.java
+++ domain/src/main/java/be/thebest/domain/repositories/PersonRepository.java
@@ -1,5 +1,8 @@
 package be.thebest.domain.repositories;
 
+import be.thebest.domain.objects.persons.Admin;
+import be.thebest.domain.objects.persons.Librarian;
+import be.thebest.domain.objects.persons.Member;
 import be.thebest.domain.objects.persons.Person;
 
 import java.util.Collections;
@@ -19,6 +22,18 @@ public class PersonRepository {
         return Collections.unmodifiableMap(personRepository);
     }
 
-    public void addPerson(Person testAdmin) {
+    public void addPerson(Person person) {
+    }
+
+    public void addMember(Member member) {
+
+    }
+
+    public void addAdmin(Admin admin) {
+
+    }
+
+    public void addLibrarian(Librarian librarian) {
+
     }
 }
