82aa412d4aed423d764373373f52a215e04e9065
reneald <delea.vanreeth@gmail.com>
2018-03-21 15:23:10 +0100

    expanded PersonControllerTest

--- api/src/test/java/be/thebest/api/PersonControllerTest.java
+++ api/src/test/java/be/thebest/api/PersonControllerTest.java
@@ -1,34 +1,31 @@
 package be.thebest.api;
 
+import be.thebest.api.admins.AdminDto;
 import be.thebest.api.admins.AdminMapper;
+import be.thebest.api.librarians.LibrarianDto;
 import be.thebest.api.librarians.LibrarianMapper;
 import be.thebest.service.PersonService;
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnit;
-import org.mockito.junit.MockitoRule;
-import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.*;
 import static org.mockito.internal.verification.VerificationModeFactory.times;
 
 public class PersonControllerTest {
-    PersonService mockService;
-    MemberMapper mockMapper;
-    AdminMapper adminMapper;
-    LibrarianMapper librarianMapper;
-    PersonController controller;
+    private PersonService mockService;
+    private MemberMapper mockMemberMapper;
+    private AdminMapper mockAdminMapper;
+    private LibrarianMapper mockLibrarianMapper;
+    private PersonController controller;
 
 
     @Before
     public void setUp() {
         mockService = Mockito.mock(PersonService.class);
-        mockMapper = Mockito.mock(MemberMapper.class);
-        controller = new PersonController(mockService, mockMapper, adminMapper, librarianMapper);
+        mockMemberMapper = Mockito.mock(MemberMapper.class);
+        mockAdminMapper = Mockito.mock(AdminMapper.class);
+        mockLibrarianMapper = Mockito.mock(LibrarianMapper.class);
+        controller = new PersonController(mockService, mockMemberMapper, mockAdminMapper, mockLibrarianMapper);
     }
 
     @Test
@@ -41,17 +38,45 @@ public class PersonControllerTest {
     public void addMember_shouldCallServiceAddMember() {
         MemberDto mockDto = Mockito.mock(MemberDto.class);
         controller.addMember(mockDto);
-        Mockito.verify(mockService, times(1)).addMember(mockMapper.toDomain(mockDto));
+        Mockito.verify(mockService, times(1)).addMember(mockMemberMapper.toDomain(mockDto));
     }
 
     @Test
     public void addMember_shouldCallMapperToDtoAndToDomain() {
         MemberDto mockDto = Mockito.mock(MemberDto.class);
         controller.addMember(mockDto);
-        Mockito.verify(mockMapper, times(1)).toDto(mockMapper.toDomain(mockDto));
-        Mockito.verify(mockMapper, times(1)).toDomain(mockDto);
-
+        Mockito.verify(mockMemberMapper, times(1)).toDto(mockMemberMapper.toDomain(mockDto));
+        Mockito.verify(mockMemberMapper, times(1)).toDomain(mockDto);
     }
 
+    @Test
+    public void addLibrarian_shouldCallServiceAddMember() {
+        LibrarianDto mockDto = Mockito.mock(LibrarianDto.class);
+        controller.addLibrarian(mockDto);
+        Mockito.verify(mockService, times(1)).addLibrarian(mockLibrarianMapper.toDomain(mockDto));
+    }
+
+    @Test
+    public void addLibrarian_shouldCallMapperToDtoAndToDomain() {
+        LibrarianDto mockDto = Mockito.mock(LibrarianDto.class);
+        controller.addLibrarian(mockDto);
+        Mockito.verify(mockLibrarianMapper, times(1)).toDto(mockLibrarianMapper.toDomain(mockDto));
+        Mockito.verify(mockLibrarianMapper, times(1)).toDomain(mockDto);
+    }
+
+    @Test
+    public void addAdmin_shouldCallServiceAddMember() {
+        AdminDto mockDto = Mockito.mock(AdminDto.class);
+        controller.addAdmin(mockDto);
+        Mockito.verify(mockService, times(1)).addAdmin(mockAdminMapper.toDomain(mockDto));
+    }
+
+    @Test
+    public void addAdmin_shouldCallMapperToDtoAndToDomain() {
+        AdminDto mockDto = Mockito.mock(AdminDto.class);
+        controller.addAdmin(mockDto);
+        Mockito.verify(mockAdminMapper, times(1)).toDto(mockAdminMapper.toDomain(mockDto));
+        Mockito.verify(mockAdminMapper, times(1)).toDomain(mockDto);
+    }
 
 }
