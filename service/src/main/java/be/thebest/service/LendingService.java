d7f4fc14818042ebc43ad78576fc437fe562ebce
reneald <delea.vanreeth@gmail.com>
2018-03-22 14:54:20 +0100

    implemented LendingService.returnBook

--- service/src/main/java/be/thebest/service/LendingService.java
+++ service/src/main/java/be/thebest/service/LendingService.java
@@ -1,12 +1,15 @@
 package be.thebest.service;
 
 import be.thebest.domain.exception.LendingException;
+import be.thebest.domain.exception.NotFoundException;
 import be.thebest.domain.objects.lendings.Lending;
 import be.thebest.domain.objects.lendings.LendingRepository;
 import be.thebest.domain.repositories.BookRepository;
+import org.omg.CosNaming.NamingContextPackage.NotFound;
 
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.time.LocalDate;
 
 @Named
 public class LendingService {
@@ -24,5 +27,18 @@ public class LendingService {
             throw new LendingException("That book is already lent out.");
         }
         return lendingRepository.addLending(lendingToAdd);
+    }
+
+    public ReturnObject returnBook(Long lendingId) {
+        if (!lendingRepository.getLendingRepository().keySet().contains(lendingId)) {
+            return ReturnObject.returnObject(ReturnObject.ERROR,
+                    "Lending not found. The book may already have been returned, or you provided a wrong lending ID.");
+        }
+        if (lendingRepository.getLending(lendingId).getDueDate(Lending.NORMAL_LENDING_PERIOD).isBefore(LocalDate.now())) {
+            return ReturnObject.returnObject(ReturnObject.OK_WITH_MESSAGE,
+                    "Book is overdue. You should have returned it by " + lendingRepository.getLending(lendingId).getDueDate(Lending.NORMAL_LENDING_PERIOD));
+        }
+        lendingRepository.removeLending(lendingId);
+        return ReturnObject.returnObject(ReturnObject.OK);
     }
 }
